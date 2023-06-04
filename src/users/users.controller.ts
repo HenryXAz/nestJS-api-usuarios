677fc9f15e5de8bd95edbd4f7cb0d76996443447
Henry Gustavo X. Az <azhenrygus@outlook.es>
2023-06-03 22:13:30 -0600

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  ParseIntPipe,
+  Delete,
+  Patch,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDTO): Promise<User> {
+    return this.userService.createUser(newUser);
+  }
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.getUser(id);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.deleteUser(id);
+  }
+
+  @Patch(':id')
+  udpateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() user: UpdateUserDTO,
+  ) {
+    return this.userService.updateUser(id, user);
+  }
+}
